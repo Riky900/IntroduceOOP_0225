e97f7592376e2a821391350a985c19fef2ec246e
Riky900 <rickytripamungkas666@gmail.com>
2025-04-28 19:17:24 +0700

    Enhance displayData

---
+++ coba3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class persegipanjang
+{
+    public:
+    float panjang ; 
+    float lebar ;
+    float luas ;
+    void inputData()
+    {
+        cout << " input panjang: ";
+        cin >> panjang ;
+        cout << " input lebar: ";
+        cin >> lebar ;
+        
+    }
+};
