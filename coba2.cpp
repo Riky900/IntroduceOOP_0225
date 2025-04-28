ef924c549e66b3e0e83f4179238be183db5fe736
Riky900 <rickytripamungkas666@gmail.com>
2025-04-28 19:15:33 +0700

    Initialize basic Planet class

---
+++ coba2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class persegipanjang
+{
+    public:
+    float panjang ; 
+    float lebar ;
+    float luas ;
+    void inputData()
+    {
+        cout << " input panjang: ";
+        cin >> panjang ;
+        cout << " input lebar: ";
+        cin >> lebar ;
+        
+    }
+    void menghitungluas(){
+        luas = panjang*lebar;
+        cout << "luas persegipanjang =" << luas << endl;
+    }
+    void output()
+    {
+        cout << "luas persegi panjang = " << luas << endl;
+    }
+};
+ int main()
+{
+    persegipanjang per ;
+    per.inputData();
+    per.menghitungluas();
+    per.output();
+    return 0;
+ 
+}
